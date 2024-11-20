2f5d974539cf2f68be2643242ba6c69373beec4b
a-beaudin <amelieb0100@gmail.com>
2024-11-20 13:39:00 -0500

    extract data to csv file for open coding

---
+++ to_csv.py
@@ -0,0 +1,22 @@
+import json
+import csv
+
+input_file = 'remaining_articles.json'
+output_file = 'articles.csv'
+
+with open(input_file, 'r') as file:
+    articles = json.load(file)
+
+with open(output_file, mode='w', newline='', encoding='utf-8') as csvfile:
+    fieldnames = ['title', 'description']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    
+    writer.writeheader()
+    
+    for article in articles:
+        writer.writerow({
+            'title': article['title'],
+            'description': article['description']
+        })
+
+print(f"Data has been written to {output_file}")
